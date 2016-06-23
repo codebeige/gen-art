08e4e2cf5c4e48e6798c4d269dfe72336286ba9e
Tibor Claassen <tibor@gmah.net>
2016-06-24 00:00:10 +0200

    [P.1.1.1-01] Rastered Color Space

---
+++ src/p_1_1_1/sketch_01.clj
@@ -0,0 +1,37 @@
+(ns p-1-1-1.sketch-01
+  (:refer-clojure :exclude [complement])
+  (:require [quil.core :as q]
+            [quil.middleware :as m]))
+
+(defn setup []
+  (q/no-cursor)
+  (q/no-stroke)
+  (q/fill 255)
+  (q/color-mode :hsb 1 1 1)
+  {:step-x 20 :step-y 25})
+
+(defn mouse-moved [state {:keys [x y]}]
+  (assoc
+   state
+   :step-x (max 2 x)
+   :step-y (max 2 y)))
+
+(defn hue-at [x]
+  (q/norm x 0 (q/width)))
+
+(defn saturation-at [y]
+  (q/norm y (q/height) 0))
+
+(defn draw [{:keys [step-x step-y]}]
+  (doseq [x (range 0 (q/width) step-x)
+          y (range 0 (q/height) step-y)]
+    (q/fill (hue-at x) (saturation-at y) 1)
+    (q/rect x y step-x step-y)))
+
+(q/defsketch rastered-color-space
+  :title "Rastered Color Space"
+  :size [800 400]
+  :middleware [m/fun-mode m/pause-on-error]
+  :setup setup
+  :draw draw
+  :mouse-moved mouse-moved)
